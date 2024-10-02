32dee36bdd0eac614f66cb33846c6d4c629fcc68
Rickert <rickert156@rambler.ru>
2024-10-02 21:02:44 +0400

    Add buffer

--- main.py
+++ main.py
@@ -1,12 +1,28 @@
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
-import smtplib, csv
-from login import log, pwd
+
+import smtplib, csv, os, shutil
+
 from letter import theme, letter
 
+ACCOUNT = 'account.csv'
 BASE_FILE = 'base.csv'
+SENT_DIR = 'Sent'
+BUFFER = 'Buffer'
 
-def main(number, recipient):
+LIMIT_LETTER = 2
+
+def writeBuffer(recipient):
+    with open(f'{BUFFER}/{recipient}', 'a') as fileR:
+        write = fileR.write(recipient)
+
+
+def createDir():
+    if not os.path.exists(SENT_DIR):os.makedirs(SENT_DIR)
+    if not os.path.exists(BUFFER):os.makedirs(BUFFER)
+
+def main(number, recipient, log, pwd):
+    writeBuffer(recipient)
     try:
         msg = MIMEMultipart()
         message = letter
@@ -32,6 +48,7 @@ def main(number, recipient):
 
         try:
             server.sendmail(msg['From'], msg['To'], msg.as_string())
+            shutil.move(f'{BUFFER}/{recipient}', f'{SENT_DIR}/{recipient}')
         except smtplib.SMTPRecipientsRefused as e:
             print(f"Ошибка отправки письма, адрес отклонен: {e}")
         except smtplib.SMTPException as e:
@@ -39,17 +56,29 @@ def main(number, recipient):
         finally:
             server.quit()
 
-        print(f"[{number}] Email sent to {msg['To']}")
+        print(f"[{number}] {log} => Email sent to {msg['To']}")
+        
 
     except Exception as e:
         print(f"Общая ошибка: {e}")
 
 def readBase():
-    with open(BASE_FILE, 'r') as file:
-        number=0
+    with open(ACCOUNT, 'r') as file:
+        number_login = 0
         for row in csv.DictReader(file):
-            number+=1
-            recipient = row['Email']
-            main(number, recipient)
+            number_login+=1
+            login = row['login']
+            password = row['password']
+            with open(BASE_FILE, 'r') as file2:
+                num_rec = 0
+                for user in csv.DictReader(file2):
+                    num_rec+=1
+                    recipient = user['Email']
+                    if recipient not in os.listdir(f'{SENT_DIR}'):
+                        main(number_login, recipient, login, password)
+                        if num_rec == LIMIT_LETTER:break
 
-readBase()
+if __name__ == '__main__':
+    createDir()
+    readBase()
+
