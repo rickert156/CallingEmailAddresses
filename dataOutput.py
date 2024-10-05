c11a3f1ed4436938b2a12e2506181a2a2c9cd587
Rickert <rickert156@rambler.ru>
2024-10-05 14:31:11 +0400

    Add data output

---
+++ dataOutput.py
@@ -0,0 +1,20 @@
+import sqlite3
+
+base = 'calling.db'
+
+def main():
+    con = sqlite3.connect(base)
+    cur = con.cursor()
+    sql = """\
+        SELECT * FROM users
+        """
+    try:
+        cur.execute(sql)
+        for id_user, email, name, domain, status, date in cur:print(f"Email: {email} | Name: {name} | Domain: {domain} | Status: {status} | Date: {date}")
+    except sqlite3.DatabaseError as ex:print(f"Error: {ex}")
+    else:print("Успех")
+
+    cur.close()
+    con.close()
+
+main()
